e0f76132ee01730186c8d7d385400a68a0cb984c
Takara0721 <2041736388@qq.com>
2025-04-25 00:34:16 +0800

    首次提交，更新chapter2

---
+++ Chapter2/decaying_epsilon_greedy.py
@@ -0,0 +1,28 @@
+from typing import Callable
+import numpy as np
+from bandit import Bandit
+from solver import Solver
+
+
+class DecayingEpsilonGreedy(Solver):
+    def __init__(
+            self,
+            bandit: Bandit,
+            decaying_epsilon: Callable[[int], int] = lambda t : 1 / t,
+            init_probability: float = 1.0
+    ) -> None:
+        super().__init__(bandit)
+        self.t = 0
+        self.decaying_epsilon = decaying_epsilon
+        self.q_list = np.array([init_probability] * self.bandit.n)
+
+    def run_one_step(self) -> int:
+        self.t += 1
+        if np.random.random() > self.decaying_epsilon(self.t):
+            k = np.argmax(self.q_list)
+        else:
+            k = np.random.randint(self.bandit.n)
+        r = self.bandit.step(k)
+        self.q_list[k] += (r - self.q_list[k]) / (self.count_list[k] + 1)
+        return k
+
---
+++ Chapter2/solver.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from bandit import Bandit
+import abc
+
+class Solver(abc.ABC):
+    def __init__(self, bandit: Bandit) -> None:
+        self.bandit = bandit
+        self.current_r = 0
+        self.count_list = np.zeros(bandit.n, dtype=np.int64)
+        self.action_list = list()
+        self.r_list = list()
+
+    def upgrade_r(self, k: int) -> None:
+        self.current_r += self.bandit.best_probability - self.bandit.probability_list[k]
+        self.r_list.append(self.current_r)
+
+    @abc.abstractmethod
+    def run_one_step(self) -> int:
+        pass
+
+    def run(self, t: int) -> None:
+        for _ in range(t):
+            k = self.run_one_step()
+            self.upgrade_r(k)
+            self.count_list[k] += 1
+            self.action_list.append(k)
